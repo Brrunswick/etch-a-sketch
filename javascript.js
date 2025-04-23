0d21490de11ff49a54c6d9ee5e33b4074ba5ae21
Brrunswick <122462882+Brrunswick@users.noreply.github.com>
2025-04-23 17:26:06 +0100

    got etch a sketch basic functions working alongside board sizing, however need to now update css to fix display with other sizes

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 // define container referencing our HTML so we can have a place to create the squares using JS
 const container = document.querySelector("#container");
+const button = document.querySelector('button');
+const input = document.querySelector('input');
 
 //a function to create a grid, for each loop it will define a variable "square" which equals a createElement function, you then add a class to the created element and then add it to the container div
 function createGrid(num) {
@@ -7,7 +9,19 @@ function createGrid(num) {
         let square = document.createElement(`div`)
         square.classList.add("square");
         container.appendChild(square);
+        square.addEventListener("mouseover", () => {
+            square.classList.add("hoverSquare");
+                  setTimeout(() => {
+            square.classList.remove("hoverSquare");
+          }, 3000);
+        })
     }
 }
 
 createGrid(256);
+
+button.addEventListener("click", () => {
+    let gridSize = input.value
+    input.value = ""
+    createGrid(gridSize)
+})
