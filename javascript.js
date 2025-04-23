5ba12e5892ec274001271bce76840e7071d30f20
Brrunswick <122462882+Brrunswick@users.noreply.github.com>
2025-04-23 10:11:32 +0100

    updated js file so that the created elements were divs

--- javascript.js
+++ javascript.js
@@ -4,7 +4,7 @@ const container = document.querySelector("#container");
 //a function to create a grid, for each loop it will define a variable "square" which equals a createElement function, you then add a class to the created element and then add it to the container div
 function createGrid(num) {
     for (i = 0; i < num; i++) {
-        let square = document.createElement(`square`)
+        let square = document.createElement(`div`)
         square.classList.add("square");
         container.appendChild(square);
     }
