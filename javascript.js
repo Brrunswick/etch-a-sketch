77938eb0b574cda8f563c55170ffa5831fbcf301
Brrunswick <122462882+Brrunswick@users.noreply.github.com>
2025-04-24 14:23:47 +0100

    rewrote JS file to create the grid using rows and then squares within the rows, instead of just squares

--- javascript.js
+++ javascript.js
@@ -6,22 +6,39 @@ const input = document.querySelector('input');
 //a function to create a grid, for each loop it will define a variable "square" which equals a createElement function, you then add a class to the created element and then add it to the container div
 function createGrid(num) {
     for (i = 0; i < num; i++) {
-        let square = document.createElement(`div`)
-        square.classList.add("square");
-        container.appendChild(square);
-        square.addEventListener("mouseover", () => {
-            square.classList.add("hoverSquare");
-                  setTimeout(() => {
-            square.classList.remove("hoverSquare");
-          }, 3000);
-        })
+        let row = document.createElement(`div`)
+        row.classList.add("row");
+        container.appendChild(row); 
+        for (j = 0; j < num; j++) {
+            let square = document.createElement(`div`)
+            square.classList.add("square");
+            row.appendChild(square);
+            square.addEventListener("mouseover", () => {
+                square.classList.add("hoverSquare");
+                      setTimeout(() => {
+                square.classList.remove("hoverSquare");
+              }, 3000);
+            })
+        }
     }
+   
 }
 
-createGrid(256);
+// a function to create a new grid on user input
 
+//looks for the button to be clicked
 button.addEventListener("click", () => {
+    //defines a variable to be given the value of the users input
     let gridSize = input.value
+    //resets the text within the input box
     input.value = ""
+    //deletes the old grid - referencing the parent as the rows and squares are only defined within the createGrid function and cannot be referenced here
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+      }
+    //runs the create grid function
     createGrid(gridSize)
 })
+
+
+createGrid(22);
